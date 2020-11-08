e47571648fc913f88888775934f0ec873ac1dbea
mcpejs <mcpejs1@gmail.com>
2020-11-08 15:24:41 +0900

    catch exception on getBitrate

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,12 @@ router.get("/view", async (req, res) => {
     <h1>올바른 링크가 아닙니다</h1>`);
     return;
   }
+
   const bitrate = await getAudioBitrate(id);
+  if (!bitrate) {
+    res.render("../views/view.ejs", { bitrate: "ERROR PLEASE TRY AGAIN", id });
+    return;
+  }
   res.render("../views/view.ejs", { bitrate, id });
 });
 
